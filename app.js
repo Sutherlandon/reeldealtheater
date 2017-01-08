decb88364042e841633e12b757123294dd8d1c71
Landon Sutherland <Sutherlandon@gmail.com>
2017-01-07 22:54:56 -0700

    Added the functionality to pull a movie by title and year on the backend. Updated the movie section to show ratings, runtime, and showtime.  Added a date selection menu to change the showtimes.

--- app.js
+++ app.js
@@ -21,6 +21,14 @@ app.get('/movie/:title', function (req, res) {
   });
 });
 
+// gets movie meta-data based on title from the imbd-api
+app.get('/movie/:title/:year', function (req, res) {
+  imdb.getReq({name: req.params.title,
+              year: req.params.year}, (err, data) => {
+    res.send(data);
+  });
+});
+
 // fires up the server to listen on port 3000
 app.listen(3000, function () {
   console.log('ReelDeal running (port: 3000)');
