d9ce21199b7fe344fb538aea8b28271727857498
Landon Sutherland <landoman@pn1708160.lanl.gov>
2018-01-01 00:22:01 -0700

    Modified date selector to run along side the Now Showing text.  Began work on movie data as a json structure/javascript object

---
+++ public/js/theater.js
@@ -0,0 +1,41 @@
+var movies = {
+  {
+    "name" : "Star Wars: Rogue One",
+    "rating" : "PG-13",
+    "runtime" : "2h 13m",
+    "showtimes" : {
+      "Friday 1/06" : {
+        "12:45 2D",
+        "3:45 2D",
+        "6:45 3D",
+        "9:45 3D"
+      },
+      "Saturday 1/07" : {
+        "12:45 2D",
+        "3:45 2D",
+        "6:45 3D",
+        "9:45 3D"
+      }
+    }
+  },
+  {
+    "name" : "Assasins Creed",
+    "rating" : "PG-13",
+    "runtime" : "2h 20m",
+    "showtimes" : {
+      "Friday 1/06" : {
+        "1:00 2D",
+        "4:00 2D",
+        "7:00 3D",
+        "9:40 3D"
+      },
+      "Saturday 1/07" : {
+        "1:00 2D",
+        "4:00 2D",
+        "7:00 3D",
+        "9:40 3D"
+      }
+    }
+  }
+}
+
